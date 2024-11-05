2480303b798edeb656eec0b0cfd6da36883ca63d
Bradley Watkins <watkinsbradley01@gmail.com>
2024-11-05 11:23:16 +0000

    courses -> lore

--- app.py
+++ app.py
@@ -29,19 +29,19 @@ def template():
 def profile(username):
     return render_template('profile.html', name=username)
 
-Courses = [
+Lores = [
     {'id': 1, 'title': 'van helsing', 'image': 'https://media.newyorker.com/photos/5909544b6552fa0be682ca71/master/pass/lincoln-vampire.jpg'}, 
     {'id': 2, 'title': 'vampire', 'image': 'https://th-thumbnailer.cdn-si-edu.com/XxlgYLkrQNjgEGJs3B1ecOh8cVs=/800x600/filters:focal(278x186:279x187)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/51/58/515809d8-1e1c-44aa-ae26-4688598f38ce/untitled-1.jpg'}, 
 ]
 
-@app.route('/api/courses')
+@app.route('/api/lores')
 def apiCourses():
-    return jsonify(Courses)
+    return jsonify(Lores)
 
 
-@app.route("/courses")
+@app.route("/lore")
 def courses():
-    return render_template("courses.html", courses=Courses)
+    return render_template("lore.html", lores=Lores)
 
 if __name__ == '__main__':
     app.run()  # If address is in use, may need to terminate other sessions:
