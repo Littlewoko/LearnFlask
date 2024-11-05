e1d651692037e05d27f05098637bc56d5d6e843b
Bradley Watkins <watkinsbradley01@gmail.com>
2024-11-05 10:55:49 +0000

    multi argument route

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def about():
 def product(name):
     return "<h3>Product</h3> <p>" + name + "</p>"
 
+@app.route("/product/<name>/<price>")
+def productPrice(name, price):
+    return "<h3>Product</h3> <p>" + name + " costs: " + price + "</p>"
 if __name__ == '__main__':
     app.run()  # If address is in use, may need to terminate other sessions:
                # Runtime > Manage Sessions > Terminate Other Sessions
