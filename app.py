eb159050c9031993acd0901cca494dff03a35135
Bradley Watkins <watkinsbradley01@gmail.com>
2024-11-05 10:48:01 +0000

    about route

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 run_with_ngrok(app)  # Start ngrok when app is run
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "index"
+
+@app.route("/about")
+def about():
+    return "This is a python web application built using Flask";
 
 if __name__ == '__main__':
     app.run()  # If address is in use, may need to terminate other sessions:
